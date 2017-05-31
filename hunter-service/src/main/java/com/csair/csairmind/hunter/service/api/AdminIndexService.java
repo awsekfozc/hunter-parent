2e5c6159775c9598925ffd3bda764a298f024ff2
ÃüÔË <305855967@qq.com>
2017-05-31 16:17:28 +0800

    修改服务接口

--- hunter-service/src/main/java/com/csair/csairmind/hunter/service/api/AdminIndexService.java
+++ hunter-service/src/main/java/com/csair/csairmind/hunter/service/api/AdminIndexService.java
@@ -4,14 +4,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by xianyongjie on 2016/11/8.
+ * Created by zhangcheng
  */
 @RestController
 @Slf4j
@@ -20,10 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 public class AdminIndexService {
 
 
-    @SuppressWarnings("unchecked")
 	@ApiOperation(value = "IP测试")
     @GetMapping("/resource")
-    public void resource(@RequestHeader("token") String token,String sysCode, HttpServletResponse httpServletResponse){
+    public void resource(String sysCode, HttpServletResponse httpServletResponse){
 
     }
 }
